1921e9fee0292c2c516659d36799bca49a05b331
张保良 <jfreebird@gmail.com>
2012-09-27 12:10:19 +0800

    edit

--- tools.rb
+++ tools.rb
@@ -1,14 +1,20 @@
 #!/usr/bin/ruby
 
 require "yaml"
+require "sequel"
 YAML::ENGINE.yamler = 'syck'
 
 #Tools Module for this!
 module Tools
   
   #Get config by file.
-  def get_config 
-    return YAML::load File.open "./config.yaml"
+  def get_config config_file
+    return YAML::load File.open config_file
   end
   
+  def get_db 
+  	#Get pg data source.
+  	Sequel.connect('postgres://postgres:dev@localhost/life_dev') 
+  end
+
 end
