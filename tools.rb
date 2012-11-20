7706d0668cbf32a31c1bc5907b045000532838ff
张保良 <jfreebird@gmail.com>
2012-11-20 15:00:37 +0800

    计算电影分数

--- tools.rb
+++ tools.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/ruby
-
 require "yaml"
 YAML::ENGINE.yamler = 'syck'
-
 #Tools Module for this!
 module Tools
   
@@ -10,5 +8,16 @@ module Tools
   def get_config config_file
     return YAML::load File.open config_file
   end
-  
+  #Get Move name by qvod url
+  def get_name_by_url(url, name)
+    temp = url.split("|")
+      temp = temp[temp.length-2].split(".")[0]
+      if temp.split(name).length > 1
+        temp.split(name)[1].replace("_", "")  
+      else
+        return temp
+      end
+  end  
+   
+   
 end
